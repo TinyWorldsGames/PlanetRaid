6de62b0a97cc0c09edc37b8db5a928c40005f403
FatihAslan388 <69825782+FatihAslan388@users.noreply.github.com>
2023-03-23 06:28:07 +0300

    Player

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+    private Vector2 movement;
+
+    private Rigidbody rb;
+    private Animator anim;
+
+    public Camera cam;
+
+    private Vector3 mousePos;
+
+    public float turnSpeed = 10f;
+
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        movement = value.Get<Vector2>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction = new Vector3(movement.x, 0f, movement.y);
+        rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+
+        mousePos = cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.transform.position.y));
+        Vector3 lookDirection = mousePos - transform.position;
+        lookDirection.y = 0;
+        Quaternion newRotation = Quaternion.LookRotation(lookDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, turnSpeed * Time.deltaTime);
+
+
+    }
+}
