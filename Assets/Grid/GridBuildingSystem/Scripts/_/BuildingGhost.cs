fdc9a6df7c47242887eb881b9d49fab8392f0160
Furkan Sert <fssertfs@gmail.com>
2023-04-09 18:54:08 +0300

    Grid Level

--- Assets/Grid/GridBuildingSystem/Scripts/_/BuildingGhost.cs
+++ Assets/Grid/GridBuildingSystem/Scripts/_/BuildingGhost.cs
@@ -36,7 +36,7 @@ public class BuildingGhost : MonoBehaviour {
     private void Update()
     {
         Vector3 targetPosition = GridBuildingSystem3D.Instance.GetMouseWorldSnappedPosition();
-        targetPosition.y = 1f;
+        targetPosition.y = 0f;
         transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * 15f);
 
         transform.rotation = Quaternion.Lerp(transform.rotation, GridBuildingSystem3D.Instance.GetPlacedObjectRotation(), Time.deltaTime * 15f);
